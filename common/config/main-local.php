14109233f29d607ac7d9804297bd7f8c005e180e
mar5 <hrvoje.orejas@gmail.com>
2018-01-17 11:00:35 +0100

    Testing gii migratons

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -1,45 +1,48 @@
 <?php
 return [
-    'components' => [
-      'db' => [
-  'class' => 'yii\db\Connection',
-  'dsn' => 'pgsql:dbname=postgres;host=db;',
-  'username' => 'postgres',
-  'password' => '=',
-  'charset' => 'utf8',
-  'schemaMap' => [
-      'pgsql' => [
+  'components' => [
+    'db' => [
+      'class' => 'yii\db\Connection',
+      'dsn' => 'pgsql:dbname=postgres;host=127.0.0.1;',
+      'username' => 'postgres',
+      'password' => '=',
+      'charset' => 'utf8',
+      'schemaMap' => [
+        'pgsql' => [
           'class' => 'tigrov\pgsql\Schema',
           'defaultSchema' => 'public',
+        ],
       ],
-  ],
-  'on afterOpen' => function ($event) {
-      $event->sender->createCommand("SET search_path TO public;")->execute();
-  },
-],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'viewPath' => '@common/mail',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
-        ],
-        'view' => [
-            'class' => 'yii\web\View',
-            'renderers' => [
-                'twig' => [
-                    'class' => 'yii\twig\ViewRenderer',
-                    'cachePath' => '@runtime/Twig/cache',
-                    // Array of twig options:
-                    'options' => [
-                        'auto_reload' => true,
-                    ],
-                    'globals' => [
-                        'html' => ['class' => '\yii\helpers\Html'],
-                    ]
-                ],
-            ],
-        ],
+      'on afterOpen' => function ($event) {
+          $event->sender->createCommand("SET search_path TO public;")->execute();
+      },
     ],
+    'mailer' => [
+      'class' => 'yii\swiftmailer\Mailer',
+      'viewPath' => '@common/mail',
+      // send all mails to a file by default. You have to set
+      // 'useFileTransport' to false and configure a transport
+      // for the mailer to send real emails.
+      'useFileTransport' => true,
+    ],
+    'view' => [
+      'class' => 'yii\web\View',
+      'renderers' => [
+        'twig' => [
+          'class' => 'yii\twig\ViewRenderer',
+          'cachePath' => '@runtime/Twig/cache',
+          // Array of twig options:
+          'options' => [
+            'auto_reload' => true,
+          ],
+          'globals' => [
+            'html' => ['class' => '\yii\helpers\Html'],
+          ]
+        ],
+      ],
+    ],
+    'utility' => [
+      'class' => 'c006\utility\migration\Module',
+    ],
+  ],
 ];
---
+++ console/migrations/m180117_095417_initial_Mass.php
@@ -0,0 +1,97 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m180117_095417_initial_Mass extends Migration
+{
+
+    public function init()
+    {
+        $this->db = 'db';
+        parent::init();
+    }
+
+    public function safeUp()
+    {
+        $tableOptions = 'ENGINE=InnoDB';
+
+        $this->createTable('{{%animal}}',[
+            'id'=> $this->primaryKey(),
+            'name'=> $this->string(255),
+            'vaccinate'=> $this->boolean(),
+            'created_at'=> $this->text()->notNull(),
+            'updated_at'=> $this->text(),
+            'birthday'=> $this->timestamp(-4),
+            'image'=> $this->text(),
+            'nickname'=> $this->text(),
+            'description'=> $this->text(),
+            'sex_male'=> $this->boolean(),
+            'approved'=> $this->boolean(),
+            'featured'=> $this->boolean(),
+            'foster_care'=> $this->boolean(),
+            'parasite'=> $this->boolean(),
+            'castrated'=> $this->boolean(),
+            'rabies'=> $this->boolean(),
+            'infectious_diseases'=> $this->boolean(),
+            'gallery'=> "varchar[]",
+            'type'=> $this->text(),
+            'breed'=> $this->text(),
+            'specie'=> $this->decimal(),
+        ], $tableOptions);
+
+
+        $this->createTable('{{%breed}}',[
+            'id'=> $this->bigPrimaryKey(),
+            'name'=> $this->text(),
+            'fk_species'=> $this->bigInteger(64),
+        ], $tableOptions);
+
+
+        $this->createTable('{{%migration}}',[
+            'version'=> $this->string(180)->notNull(),
+            'apply_time'=> $this->integer(32),
+        ], $tableOptions);
+
+        $this->addPrimaryKey('pk_on_migration','{{%migration}}',['version']);
+
+        $this->createTable('{{%species}}',[
+            'id'=> $this->bigPrimaryKey(),
+            'name'=> $this->text(),
+        ], $tableOptions);
+
+
+        $this->createTable('{{%user}}',[
+            'id'=> $this->primaryKey(),
+            'username'=> $this->string(255)->notNull(),
+            'auth_key'=> $this->string(32)->notNull(),
+            'password_hash'=> $this->string(255)->notNull(),
+            'password_reset_token'=> $this->string(255),
+            'email'=> $this->string(255)->notNull(),
+            'status'=> $this->smallInteger(16)->notNull()->defaultValue(10),
+            'created_at'=> $this->integer(32)->notNull(),
+            'updated_at'=> $this->integer(32)->notNull(),
+        ], $tableOptions);
+
+        $this->createIndex('user_email_key','{{%user}}',['email'],true);
+        $this->createIndex('user_password_reset_token_key','{{%user}}',['password_reset_token'],true);
+        $this->createIndex('user_username_key','{{%user}}',['username'],true);
+        $this->addForeignKey(
+            'fk_breed_fk_species',
+            '{{%breed}}', 'fk_species',
+            '{{%species}}', 'id',
+            'CASCADE', 'CASCADE'
+        );
+    }
+
+    public function safeDown()
+    {
+            $this->dropForeignKey('fk_breed_fk_species', '{{%breed}}');
+            $this->dropTable('{{%animal}}');
+            $this->dropTable('{{%breed}}');
+            $this->dropPrimaryKey('pk_on_migration','{{%migration}}');
+            $this->dropTable('{{%migration}}');
+            $this->dropTable('{{%species}}');
+            $this->dropTable('{{%user}}');
+    }
+}
